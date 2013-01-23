81b4acdd199cf9fc78b9947ca5d307123703f940
nicoechaniz <nicoechaniz@altermundi.net>
2013-01-23 05:01:34 -0300

    completed initial set of tests for the data generator

--- _attachments/js/AlterMap.js
+++ _attachments/js/AlterMap.js
@@ -9,42 +9,109 @@ AlterMap.addRegions({
 });
 
 AlterMap.Network = Backbone.Model.extend({
-    url : function() {
-        return this.id ? '/networks/' + this.id : '/networks';
-    },
+  url : function() {
+    return this.id ? '/networks/' + this.id : '/networks';
+  },
+
+  nodeCount: function(){
+  }
 });
 
 AlterMap.Zone = Backbone.Model.extend({
-    url : function() {
-        return this.id ? '/zones/' + this.id : '/zones';
-    },
+  url : function() {
+    return this.id ? '/zones/' + this.id : '/zones';
+  },
 });
 
-
 AlterMap.Node = Backbone.Model.extend({
-    url : function() {
-        // POST to '/nodes' and PUT to '/nodes/:id'
-        return this.id ? '/nodes/' + this.id : '/nodes';
+  url : function() {
+    // POST to '/nodes' and PUT to '/nodes/:id'
+    return this.id ? '/nodes/' + this.id : '/nodes';
+  },
+
+  /*
+    select: function(){
+    if (!this.get("selected")){
+    this.set({selected: true}, {silent: true});
+    this.trigger("selected");
+    this.collection.select(this);
+    }
+    AlterMap.vent.trigger("node:selected", this);
     },
+
+    deselect: function(){
+    this.unset("selected", {silent: true});
+    this.trigger("deselected");
+    }
+  */
 });
 
 AlterMap.Device = Backbone.Model.extend({
-    url : function() {
-        return this.id ? '/devices/' + this.id : '/devices';
-    },
+  url : function() {
+    return this.id ? '/devices/' + this.id : '/devices';
+  },
 });
 
 AlterMap.Interface = Backbone.Model.extend({
-    url : function() {
-        return this.id ? '/interfaces/' + this.id : '/interfaces';
-    },
+  url : function() {
+    return this.id ? '/interfaces/' + this.id : '/interfaces';
+  },
 });
 
-AlterMap.WifiLink = Backbone.Model.extend({
-    url : function() {
-        return this.id ? '/wifilinks/' + this.id : '/wifilinks';
-    },
+AlterMap.Wifilink = Backbone.Model.extend({
+  url : function() {
+    return this.id ? '/wifilinks/' + this.id : '/wifilinks';
+  },
 });
+
+AlterMap.NetworkCollection =  Backbone.Collection.extend({
+  db : {
+    changes : true
+  },
+  url: "/networks",
+  model: AlterMap.Network,
+});
+
+AlterMap.ZoneCollection =  Backbone.Collection.extend({
+  db : {
+    changes : true
+  },
+  url: "/zones",
+  model: AlterMap.Zone,
+});
+
+AlterMap.NodeCollection =  Backbone.Collection.extend({
+  db : {
+    changes : true
+  },
+  url: "/nodes",
+  model: AlterMap.Node,
+});
+
+AlterMap.DeviceCollection =  Backbone.Collection.extend({
+  db : {
+    changes : true
+  },
+  url: "/devices",
+  model: AlterMap.Device,
+});
+
+AlterMap.InterfaceCollection =  Backbone.Collection.extend({
+  db : {
+    changes : true
+  },
+  url: "/interfaces",
+  model: AlterMap.Interface,
+});
+
+AlterMap.WifilinkCollection =  Backbone.Collection.extend({
+  db : {
+    changes : true
+  },
+  url: "/wifilinks",
+  model: AlterMap.Wifilink,
+});
+
 
 AlterMap.Router = Backbone.Router.extend({
   routes: {
@@ -67,14 +134,29 @@ AlterMap.addInitializer(function(options){
 
 
   var nodes = new AlterMap.NodeCollection();
-  console.log("initialized");
 
+/*
+  nodes.on("add", function(node){
+    AlterMap.addNode(node);
+  });
+
+  AlterMap.vent.on("node:selected", function(node){
+    AlterMap.showNode(node);
+    router.navigate("nodes/" + node.id);
+  });
+
+  var nodeListView = new AlterMap.NodeListView({
+    collection: nodes
+  });
+  nodeListView.render();
+
+  $("#node-list").html(nodeListView.el);
+*/
   var router = new AlterMap.Router({
     collection: nodes
   });
 
 });
-
 
 AlterMap.on("initialize:after", function(){
 //  Backbone.history.start();
--- _attachments/test/datagen.js
+++ _attachments/test/datagen.js
@@ -5,8 +5,8 @@ var CENTER_COORDS = {
 
 var randomString = function() {
   var result = [];
-  strLength = 13;
-  charSet = 'ABCDEF0123456789';
+  var strLength = 13;
+  var charSet = 'ABCDEF0123456789';
   while (--strLength) {
     result.push(charSet.charAt(Math.floor(Math.random() * charSet.length)));
   }
@@ -18,42 +18,47 @@ var randomMAC = function() {
 }
 
 var randomCoords = function(coords) {
-  coords = coords || CENTER_COORDS;
-  lon_str = coords.lon.toString();
-  lat_str = coords.lat.toString();
-  lon_base = lon_str.slice(0, lon_str.indexOf('.')+3);
-  lat_base = lat_str.slice(0, lat_str.indexOf('.')+3);
-  lon_variation = Math.floor(Math.random()*8999999+1000000).toString();
-  lat_variation = Math.floor(Math.random()*8999999+1000000).toString();
-  rand_lon = parseFloat(lon_base+lon_variation);
-  rand_lat = parseFloat(lat_base+lat_variation);
-  coords = {lon: rand_lon, lat: rand_lat}
+  var coords = coords || CENTER_COORDS;
+  var lon_str = coords.lon.toString();
+  var lat_str = coords.lat.toString();
+  var lon_base = lon_str.slice(0, lon_str.indexOf('.')+3);
+  var lat_base = lat_str.slice(0, lat_str.indexOf('.')+3);
+  var lon_variation = Math.floor(Math.random()*8999999+1000000).toString();
+  var lat_variation = Math.floor(Math.random()*8999999+1000000).toString();
+  var rand_lon = parseFloat(lon_base+lon_variation);
+  var rand_lat = parseFloat(lat_base+lat_variation);
+  var coords = {lon: rand_lon, lat: rand_lat};
   return coords
+}
+
+var randomSignal = function(){
+  return -Math.floor(Math.random()*100)
 }
 
 var DataGen = {
   generateNetwork: function(attrs){
-    attrs = attrs || {}
-    network_name = attrs.name || randomString()+'Libre';
-    completed_attrs = {
-      _id: _.uniqueId('network_').toString(),
+    attrs = attrs || {};
+    var id = attrs.id || _.uniqueId('network_').toString();
+    var network_name = attrs.name || randomString()+'Libre';
+    var coords = attrs.coords || CENTER_COORDS;
+    var completed_attrs = {
+      _id: id,
       name: network_name,
+      coords: coords
     }
-    if(attrs.coords != undefined){
-      completed_attrs.coords = attrs.coords
-    }
-    net = new AlterMap.Network(completed_attrs)
-    net.save()
-    return net;
+    var network = new AlterMap.Network(completed_attrs);
+    network.save();
+    return network;
   },
 
-  generateZone: function(name){
-    zone_name = name || '';
-    network = this.generateNetwork();
-    zone = new AlterMap.Zone({
+  generateZone: function(attrs){
+    attrs = attrs || {};
+    var zone_name = attrs.name || '';
+    var network_id = attrs.network_id || this.generateNetwork().get('_id');
+    var zone = new AlterMap.Zone({
       _id: _.uniqueId('zone_').toString(),
       name: zone_name,
-      network_id: network.get('_id'),
+      network_id: network_id,
     });
     zone.save()
     return zone;
@@ -61,56 +66,92 @@ var DataGen = {
 
   generateNode: function(attrs){
     attrs = attrs || {};
-    coords = attrs.coords || randomCoords();
-    node_name = attrs.name || randomString()+'_node';
-    zone = this.generateZone();
-    completed_attrs = {
+    var coords = attrs.coords || randomCoords();
+    var node_name = attrs.name || randomString()+'_node';
+    var zone_id = attrs.zone_id || this.generateZone().get('_id');
+    var completed_attrs = {
       _id: _.uniqueId('node_').toString(),
       name: node_name,
       coords: coords,
       elevation: 50,
-      zone_id: zone.get('_id'),
+      zone_id: zone_id,
     }
-    node = new AlterMap.Node(completed_attrs);
+    var node = new AlterMap.Node(completed_attrs);
     node.save()
     return node;
   },
 
-  generateDevice: function(){
-    node = this.generateNode();
-    device = new AlterMap.Device({
+  generateDevice: function(attrs){
+    attrs = attrs || {};
+    var node_id = attrs.node_id || this.generateNode().get('_id');
+    var device = new AlterMap.Device({
       _id: _.uniqueId('device_').toString(),
-      node_id: node.get('_id'),
+      node_id: node_id
     });
-    device.save()
+    device.save();
     return device;
   },
 
-  generateInterface: function(){
-    device = this.generateDevice();
-    interface = new AlterMap.Interface({
+  generateInterface: function(attrs){
+    attrs = attrs || {};
+    var device_id = attrs.device_id || this.generateDevice().get('_id');
+    var iface = new AlterMap.Interface({
       _id: _.uniqueId('interface_').toString(),
       name: 'wlan0',
       phydev: 'phy0',
       macaddr: randomMAC(),
       mode: 'adhoc',
       medium: 'wireless',
-      device_id: device.get('_id'),
+      device_id: device_id,
     });
-    interface.save()
-    return interface;
+    iface.save();
+    return iface;
   },
 
-  generateWifiLink: function(attrs){
-    wifilink = new AlterMap.WifiLink({
+  generateWifilink: function(attrs){
+    if (attrs.macaddr==undefined || attrs.station==undefined){
+      throw ('generateWifilink: missing macaddr or station');
+    }
+    if (attrs.macaddr == attrs.station){
+      throw ('generateWifilink: macaddr and station cannot be the same');
+    }
+    var wifilink = new AlterMap.Wifilink({
       _id: _.uniqueId('wifilink_').toString(),
       macaddr: attrs.macaddr,
       station: attrs.station,
-      attributes: { signal: -67, channel: 11 }
+      attributes: { signal: randomSignal(), channel: 11 }
     });
-    wifilink.save()
+    wifilink.save();
     return wifilink;
   },
 
+  generateFixture: function(attrs){
+    var node_count = attrs.node_count || 10;
+    var network = this.generateNetwork();
+    var zone = this.generateZone({network_id: network.get('_id')});
+    var nodes = new AlterMap.NodeCollection();
+    var devices = new AlterMap.DeviceCollection();
+    var interfaces = new AlterMap.InterfaceCollection();
+    var wifilinks = new AlterMap.WifilinkCollection();
+    for (var i=1; i<=node_count; i++){
+      var node = this.generateNode({zone_id: zone.get('_id')});
+      nodes.add(node);
+      var device = this.generateDevice({node_id: node.get('_id')});
+      devices.add(device);
+      var iface = this.generateInterface({device_id: device.get('_id')})
+      interfaces.add(iface);
+      if (i>1){
+        prev_node_id = nodes.at(i-2).get('_id');
+        prev_device_id = devices.where({node_id: prev_node_id})[0].get('_id');
+        prev_iface = interfaces.where({device_id: prev_device_id})[0];
+        var wifilink = this.generateWifilink({
+          macaddr: prev_iface.get('macaddr'),
+          station: iface.get('macaddr')
+        });
+        wifilinks.add(wifilink);
+      }
+    }
+    return {network: network, zone: zone, nodes: nodes, wifilinks: wifilinks};
+  },
 
 }
