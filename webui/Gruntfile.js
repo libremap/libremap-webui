db68cb692d72f20296fac2675d8b70c116fb88b6
André Gaul <gaul@web-yard.de>
2013-10-08 00:24:33 +0200

    [webui] grunt watch+jshint

--- webui/Gruntfile.js
+++ webui/Gruntfile.js
@@ -1,6 +1,9 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      files: ['src/js/**/*.js']
+    },
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -11,9 +14,9 @@ module.exports = function(grunt) {
       }
     }
   });
-  // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['jshint']);
 };
