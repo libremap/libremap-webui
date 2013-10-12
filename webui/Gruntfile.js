eb28051a1dab1f8e9e2119190dbee739b39a6a4b
André Gaul <gaul@web-yard.de>
2013-10-12 12:26:09 +0200

    [webui] missing files

--- webui/Gruntfile.js
+++ webui/Gruntfile.js
@@ -1,9 +1,11 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    // lint js files
     jshint: {
       files: ['src/js/**/*.js']
     },
+    // minify js files
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -13,27 +15,58 @@ module.exports = function(grunt) {
         dest: 'build/<%= pkg.name %>.min.js'
       }
     },
+    // copy static files
     copy: {
       webui: {
         files: [
           {
             expand: true,
             cwd: 'src',
-            src: [ '*.html', 'images', 'css'],
+            src: [ '*.html', 'images/**', 'css/**'],
             dest: 'build/'
           }
         ]
       }
     },
     concat: {
+      // concat vendor css files
       vendor: {
         files: {
-          'build/js/vendor.js': [
-            'bower_components/bootstrap/dist/js/bootstrap.min.js'
-          ],
-          'build/css/vendor.css': [
-            'bower_components/bootstrap/dist/css/bootstrap.min.css'
+          'build/vendor/vendor.css': [
+            'bower_components/bootstrap/dist/css/bootstrap.min.css',
+            'vendor/leaflet/leaflet.css'
           ]
+        }
+      }
+    },
+    browserify: {
+      // browserify libremap.js -> bundle.js
+      libremap: {
+        dest: 'build/js/libremap.js',
+        src: [ 'src/js/libremap.js' ],
+        options: {
+          debug: grunt.option('debug'),
+          external: ['jquery', 'bootstrap', 'leaflet']
+        }
+      },
+      vendor: {
+        dest: 'build/vendor/vendor.js',
+        src: [],
+        options: {
+          shim: {
+            jquery: {
+              path: 'bower_components/jquery/jquery.min.js',
+              exports: '$'
+            },
+            bootstrap: {
+              path: 'bower_components/bootstrap/dist/js/bootstrap.min.js',
+              exports: 'bootstrap'
+            },
+            leaflet: {
+              path: 'vendor/leaflet/leaflet.js',
+              exports: 'L'
+            },
+          }
         }
       }
     }
@@ -42,7 +75,8 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-browserify');
 
   // Default task(s).
-  grunt.registerTask('default', ['jshint', 'copy']);
+  grunt.registerTask('default', ['copy', 'concat', 'browserify']);
 };
