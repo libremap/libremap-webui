4b3785a79696a5a955dfd39df3d2885e39300372
André Gaul <gaul@web-yard.de>
2013-10-12 12:17:49 +0200

    [webui] use grunt + bower + browserify

---
+++ webui/src/js/libremap.js
@@ -0,0 +1,12 @@
+(function() {
+  // include the shimmed modules first
+  // (they make trouble otherwise)
+  var $ = require('jquery');
+  var bootstrap = require('bootstrap');
+  var L = require('leaflet');
+
+  Backbone = require('backbone');
+  var _ = require('underscore');
+
+  console.log(Backbone);
+})();
