08fcd5cc4903d6942b1a36b1dd20b1ac274668de
André Gaul <andre@gaul.io>
2015-04-06 01:12:04 +0200

    remove resize code

--- src/js/controllers/map.js
+++ src/js/controllers/map.js
@@ -5,12 +5,15 @@ var angular = require('angular');
 module.exports = function(app) {
   app.controller('mapCtrl', [
     '$scope', '$window', '$timeout', function($scope, $window, $timeout) {
+      // take care of resize events
+      // TODO: seems to be unnecessary -> remove!
+      /*
       var resize = function() {
         $scope.$broadcast('invalidateSize');
       };
-
       angular.element($window).on('resize', resize);
       $timeout(resize);
+      */
     }
   ]);
 };
